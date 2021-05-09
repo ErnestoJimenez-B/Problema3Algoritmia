b35de7a41c94f3c2416a51e6da07a8d6cc800ebc
ErnestoJimenez-B <80132397+ErnestoJimenez-B@users.noreply.github.com>
2021-05-08 19:26:25 -0500

    Update Problema3Algoritmia.js

--- Problema3Algoritmia.js
+++ Problema3Algoritmia.js
@@ -1,6 +1,14 @@
-function problema3(){
+function Problema3(){
   var input = document.querySelector('#p3-Input').value;
   var arreglo = input.split(',');
   var posicion = "";
   var lnMayor = 0;
   var lsMayor = "";
+  for (var i = 0 ; i < arreglo.length ; i++){
+        posicion = arreglo[i];
+  var lsSalida = Conteo(posicion.replace(" ",""));
+        var lasValores = lsSalida.split("|");
+        var lnLargo = parseInt(lasValores[0],10);
+  if(lnLargo > lnMayor){
+  lnMayor = lnLargo;
+  lsMayor = lsSalida;
